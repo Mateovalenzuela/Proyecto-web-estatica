2651a240a5a937ae94c7804b0be49fc3ab734807
Mateovalenzuela <mateo.valenzuela.dev@gmail.com>
2023-07-22 03:18:25 -0300

    Agregado en formulario contacto

--- static/js/contacto.js
+++ static/js/contacto.js
@@ -8,11 +8,19 @@ function validar() {
     const apellido = document.getElementById("txt-apellido");
     const email = document.getElementById("txt-email");
     const mensaje = document.getElementById("txt-mensaje");
+    const telefono = document.getElementById("txt-telefono");
+    const asunto = document.getElementById("txt-asunto");
+    const opciones = document.getElementsByName("opciones");
 
     const nombre_validacion = document.getElementById("valid-nombre");
     const apellido_validacion = document.getElementById("valid-apellido")
     const email_validacion = document.getElementById("valid-email")
     const mensaje_validacion = document.getElementById("valid-mensaje")
+    const telefono_validacion = document.getElementById("valid-telefono");
+    const asunto_validacion = document.getElementById("valid-asunto");
+    const opciones_validacion = document.getElementById("valid-opciones");
+
+    let todoValido = true;
 
     if (validar_nombre(nombre.value)) {
         nombre_validacion.innerHTML = "Nombre válido";
@@ -20,6 +28,7 @@ function validar() {
     } else {
         nombre_validacion.innerHTML = "Nombre inválido";
         nombre_validacion.className = "invalido";
+        todoValido = false;
     }
 
     if (validar_apellido(apellido.value)) {
@@ -28,6 +37,7 @@ function validar() {
     } else {
         apellido_validacion.innerHTML = "Apellido inválido";
         apellido_validacion.className = "invalido";
+        todoValido = false;
     }
 
     if (validar_email(email.value)) {
@@ -36,6 +46,7 @@ function validar() {
     } else {
         email_validacion.innerHTML = "Email inválido";
         email_validacion.className = "invalido";
+        todoValido = false;
     }
 
     if (validar_mensaje(mensaje.value)) {
@@ -44,7 +55,50 @@ function validar() {
     } else {
         mensaje_validacion.innerHTML = "Mensaje inválido";
         mensaje_validacion.className = "invalido";
+        todoValido = false;
     }
+
+    if (validar_telefono(telefono.value)) {
+        telefono_validacion.innerHTML = "Teléfono válido";
+        telefono_validacion.className = "valido";
+    } else {
+        telefono_validacion.innerHTML = "Teléfono inválido";
+        telefono_validacion.className = "invalido";
+        todoValido = false;
+    }
+
+    if (validar_opcion(asunto.value)){
+        asunto_validacion.innerHTML = "Opción válida";
+        asunto_validacion.className = "valido";
+    }else{
+        asunto_validacion.innerHTML = "Seleccione una opción";
+        asunto_validacion.className = "invalido";
+        todoValido = false;
+    }
+
+    if (validar_radio(opciones)){
+        opciones_validacion.innerHTML = "Opción válida";
+        opciones_validacion.className = "valido";
+    }else{
+        opciones_validacion.innerHTML = "Seleccione una opción";
+        opciones_validacion.className = "invalido";
+        todoValido = false;
+    }
+
+
+    if (todoValido) {
+        const btnGuardar = document.getElementById('btn-enviar-form');
+
+        btnGuardar.textContent = '\u2713'; // Unicode checkmark symbol
+        btnGuardar.classList.toggle('round');
+        setTimeout(function () {
+            btnGuardar.classList.toggle('check');
+        }, 500); // La animación de transformación tarda 0.5 segundos (500ms)
+    }
+
+
+
+
 
 }
 
@@ -67,4 +121,30 @@ function validar_apellido(apellido) {
 function validar_mensaje(mensaje) {
     const patron = /^[a-zA-Z]{4,200}$/;
     return patron.test(mensaje);
+}
+
+function validar_telefono(telefono) {
+    const patron = /^[0-9]{10}$/;
+    return patron.test(telefono);
+  }
+
+function validar_opcion(opcion) {
+    if (opcion !== "0"){
+        console.log(opcion);
+        return true;
+    }
+    return false;
+}
+
+function validar_radio(opciones){
+    let valorSeleccionado = '';
+    opciones.forEach(opcion => {
+        if (opcion.checked) {
+          valorSeleccionado = opcion.value;
+        }
+      });
+    if (valorSeleccionado !== ''){
+        return true;
+    }
+    return false;
 }
